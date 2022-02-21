55c1e258b9c8898d08a0b3b9fe445ece82e7b8e5
Sinisa Jovanovic <73884960+SinisaJO@users.noreply.github.com>
2022-02-21 20:12:23 +0100

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,5 +1,4 @@
 // DARK/LIGHT MODE
-
 const toggle = document.querySelector(".toggle");
 const circle = document.getElementById("circle");
 const border = document.querySelector(".card");
@@ -7,15 +6,18 @@ const card = document.querySelector(".card__description");
 //local storage theme
 let darkMode = localStorage.getItem("darkMode");
 
+let white = "#FBF7EE";
+let dark = "#1C1C1C";
+let darkSecondary = "#1F2937";
+
 const enableDarkMode = () => {
     circle.classList.remove("circle-light"); 
     circle.classList.add("circle-dark");
-    toggle.style.backgroundColor = "#FBF7EE";
-    document.body.style.backgroundColor = "#1C1C1C";
-    border.style.borderColor = "#FBF7EE";
-    card.style.backgroundColor = "#1F2937";
-    card.style.color = "#FBF7EE";
-    
+    toggle.style.backgroundColor = white;
+    document.body.style.backgroundColor = dark;
+    border.style.borderColor = white;
+    card.style.backgroundColor = darkSecondary;
+    card.style.color = white;
 
     localStorage.setItem("darkMode", "enabled")
 } 
@@ -23,11 +25,11 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
     circle.classList.remove("circle-dark");
     circle.classList.add("circle-light");
-    toggle.style.backgroundColor = "#1C1C1C";
-    document.body.style.backgroundColor = "#FBF7EE";
-    border.style.borderColor = "#1F2937";
-    card.style.backgroundColor = "#FBF7EE";
-    card.style.color = "#1F2937";
+    toggle.style.backgroundColor = dark;
+    document.body.style.backgroundColor = white;
+    border.style.borderColor = darkSecondary;
+    card.style.backgroundColor = white;
+    card.style.color = darkSecondary;
 
     localStorage.setItem("darkMode", null);
 } 
@@ -40,10 +42,8 @@ toggle.addEventListener('click', () => {
     darkMode = localStorage.getItem("darkMode");
     if (darkMode !== "enabled"){
         enableDarkMode();
-        console.log(darkMode);
     }else {
         disableDarkMode();
-        console.log(darkMode);
     }
 })
 
